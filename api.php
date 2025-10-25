960d400cb2fb105db08dfbadc20a2186da6d1c70
AlanGomez2022 <alanagustingomez@gmail.com>
2025-10-25 10:12:33 -0300

    Reto Dia 1: saludo dinamico con parameto GET

--- api.php
+++ api.php
@@ -1,9 +1,12 @@
 <?php
 header ('Content_Type: application/jsonn');
 
+$name = isset($_GET['name']) ? $_GET['name']:"Padawan";
+
+
 $response =[
     'satus' => 'ok',
-    'message' => 'Hola, el web service está vivo'
+    'message' => 'Hola $name, el web service está vivo'
 ];
 
 echo json_encode($response);

84886a599672a4c3495206f079f5b2a81fbbbed1
alanagustingomez@gmail.com <alanagustingomez@gmail.com>
2025-10-25 10:36:08 -0300

    Dia 1: cambios en message del php

--- api.php
+++ api.php
@@ -6,7 +6,7 @@ $name = isset($_GET['name']) ? $_GET['name']:"Padawan";
 
 $response =[
     'satus' => 'ok',
-    'message' => 'Hola $name, el web service está vivo'
+    'message' => 'Hola '. $name.', el web service está vivo'
 ];
 
 echo json_encode($response);
